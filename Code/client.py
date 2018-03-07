dd6c57343996f4ebb61a631a82121ee54d3f9459
Jared Ping <704447@students.wits.ac.za>
2018-03-07 21:41:58 +0200

    CLI client to test major FTP server functionality

--- Code/client.py
+++ Code/client.py
@@ -1,19 +1,335 @@
-#!/usr/bin/python           				# This is client.py file
-				
-import socket               				# Import socket module
-	
-### TCP SOCKET ### 	
-s = socket.socket()         				# Create a socket object
-host = socket.gethostname() 				# Get local machine name
-port = 8000               				# Reserve a port for your service.
+import socket
+import sys
+import os
+import time
+from time import sleep
+from os import walk
 
-s.connect((host, port))
-print(s.recv(1024).decode("utf-8"))
-while True:
-	message = input('Enter a message: ')                # Obtain message to send
-	s.send(bytes(message, 'UTF-8'))
-	print(s.recv(1024).decode("utf-8"))
-	if message == 'exit':			       # Close connection is exit command sent
-		print('ending session')
+s = socket.socket()
+
+def send(mes=''):
+	s.send(bytes(mes + ("\r\n"), "UTF-8"))
+
+def recieve():
+	rec = s.recv(1024)
+	return (rec)
+	    
+def action(mes=''):
+	send(mes)
+	return recieve()
+
+def local_dir(path=''):
+	for (dirpath, dirnames, filenames) in walk(path):
+		print ('"'+dirpath+'"')
 		break
-s.close()						   # Close the socket when done
+
+def browse_local(path=''):
+	for (dirpath, dirnames, filenames) in walk(path):
+		print (dirnames)
+		print (filenames)
+		print ('\n')
+		break
+
+def pasv():
+	while True:
+		vali = ''
+		mes = ('PASV')
+		send(mes)
+		mes = (s.recv(1024))
+		mes = mes.decode()
+		nmsg = mes.split('(')
+		nmsg = nmsg[-1].split(')')
+		p = nmsg[0].split(',')
+		newip = '.'.join(p[:4])
+		newport = int(p[4])*256 + int(p[5])
+		return (newip,newport)
+		break
+		
+def sendfile(file=''):
+	newip, newport = pasv()
+	p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	p.connect((newip, newport))
+	send('STOR '+file)
+	filepath = path + '/' + file
+	f = open(filepath 'rb')
+	size = os.stat(filepath)[6]
+	opened = True
+	pos = 0
+	buff = 1024
+	packs = size/1024
+	timeb = 100/packs
+	i=0
+
+	while opened:
+		i=i+timeb
+
+		if i>100:
+			i=100
+
+		time.sleep(.05)
+		sys.stdout.write("\r%d%%" %i)
+		sys.stdout.flush()
+		f.seek(pos)
+		pos += buff
+
+		if pos >= size:
+			piece = f.read(-1)
+			opened = False
+		else:
+			piece = f.read(buff) 
+		p.send(piece)
+		
+		f.seek(pos)
+	f.close()
+	recieve()
+
+def recievefile(file=''):
+	newip, newport = pasv()
+	p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	p.connect((newip, newport))
+	action('RETR '+file)
+	newfile = open(file, 'w')
+	msg=''
+	aux=':)'
+
+	while aux != "":
+		time.sleep(.05)
+		sys.stdout.write("\r" "wait")
+		sys.stdout.flush()
+		aux = p.recv(1024)
+		aux=aux.decode(errors='ignore')
+		newfile.write(aux)
+	newfile.close()
+
+def listar():
+	newip, newport = pasv()
+	p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	p.connect((newip, newport))
+	mes = ('NLST')
+	action (mes)
+	rec = p.recv(1024)
+	rec = rec.decode()
+	rec.split('\r\n')
+	print(rec)
+	mes = ('ABOR')
+	p.send(bytes(mes + ("\r\n"), "UTF-8"))
+	recieve()
+
+address = input("Enter FTP Address: ")
+port = int(input("Enter FTP Port: "))
+s.connect((address, port))
+s.recv(1024)		
+		
+action('USER '+'my_name_is_jeff')
+action('PASS '+'strongpassword')
+
+path = os.getcwd()
+buff=1024
+
+while True:
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print ('1 - Print Local and Remote Directory')
+	print ('2 - Change Directory')
+	print ('3 - Send Files')
+	print ('4 - Recieve Files')
+	print ('5 - Change Permissions')
+	print ('6 - Exit')
+	selection = input('Select an option: ')
+
+	if selection == '1':
+		while True:
+			directory = ''
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print('Remote Directory')
+			mes = ('PWD')
+			send(mes)
+			directory = s.recv(1024)
+			directory = directory.decode()
+			vali = directory.split('i')
+			vali = vali[0].split(' ')
+			vali = vali[0]
+
+			if vali == '257':
+				directory = directory.split('"')
+				directory = directory[1]
+				print('"'+directory+'"')
+			else:
+				print('"'+directory+'"')
+
+			listar()
+			print('\nLocal Directory')
+			local_dir(path)
+			browse_local(path)
+			print(input('Hit Return'))
+			break
+
+	if selection == '2':
+		while True:
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print ('1 - Change Local Directory')
+			print ('2 - Change Remote Directory')
+			print ('3 - Return to Previous Menu')
+			selection2 = input('Select an option: ')
+
+			if selection2 == '1':
+				path2 = input('Change local directory (write home to return to home directory): ')
+
+				if path2 == 'home':
+					path = '/home/'
+				else: 
+					path = (path+path2+'/')
+
+				browse_local(path)
+				print(input('Hit Return'))
+
+			if selection2 == '2':
+				print('Change remote directory (write up to go one directory up)')
+				rd = input("Specify directory: ")
+
+				if rd == 'up':
+					action('CDUP')
+				else:
+					action('CWD '+rd)
+
+				listar()
+				print(input('\nHit Return'))
+
+			if selection2 == '3':
+				break
+
+	if selection == '3':
+		while True:
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print ('File type')
+			print ('1 - ASCII')
+			print ('2 - Image')
+			print ('3 - Return to Previous Menu')
+			selection2 = input('Select an option: ')
+
+			if selection2 == '1':
+				os.path = path
+				file = input('File Name: ')
+				mes = ('TYPE A')
+				send(mes)
+
+				while True:
+					vali = recieve()
+					vali = vali.decode()
+					vali = vali.split("'")
+					vali = vali[0].split(' ')
+					vali = vali[0]
+
+					if vali == '226':
+						mes = ('ABOR')
+						action(mes)
+						recieve()
+						break
+					else:
+						break
+
+				action(mes)
+				sendfile(file)
+				print(input('Hit Return'))
+
+			if selection2 == '2':
+				os.path = path
+				file = input('File Name: ')
+				mes = ('TYPE I')
+				send(mes)
+				
+				while True:
+					vali = recieve()
+					vali = vali.decode()
+					vali = vali.split("'")
+					vali = vali[0].split(' ')
+					vali = vali[0]
+
+					if vali == '226':
+						mes = ('ABOR')
+						action(mes)
+						recieve()
+						break
+					else:
+						break
+
+				sendfile(file)
+				print(input('Hit Return'))
+
+			if selection2 == '3':
+				break
+
+	if selection == '4':
+		while True:
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print ('File type')
+			print ('1 - ASCII')
+			print ('2 - Image')
+			print ('3 - Return')
+			selection2 = input('Select an option: ')
+
+			if selection2 == '1':
+				os.path = path
+				file = input('File Name: ')
+				mes = ('TYPE A')
+				send(mes)
+
+				while True:
+					vali = recieve()
+					vali = vali.decode()
+					vali = vali.split("'")
+					vali = vali[0].split(' ')
+					vali = vali[0]
+
+					if vali == '226':
+						mes = ('ABOR')
+						action(mes)
+						recieve()
+						break
+					else:
+						break
+
+				recievefile(file)
+				print(input('Hit Return'))
+				
+			if selection2 == '2':
+				os.path = path
+				file = input('File Name: ')
+				mes = ('TYPE I')
+				send(mes)
+
+				while True:
+					vali = recieve()
+					vali = vali.decode()
+					vali = vali.split("'")
+					vali = vali[0].split(' ')
+					vali = vali[0]
+
+					if vali == '226':
+						mes = ('ABOR')
+						action(mes)
+						recieve()
+						break
+					else:
+						break
+
+				recievefile(file)
+				print(input('Hit Return'))
+				
+			if selection2 == '3':
+				break
+				
+	if selection == '5':
+		os.system('cls' if os.name == 'nt' else 'clear')
+		file=input('File name: ')
+		perm=input('Write permissions in hexadecimal, Example (742) :')
+		action('SITE CHMOD '+ perm + ' ' + file)
+		print('the permissions of '+ file +' has been updated to ' + perm)
+		print(input('\nHit Return'))
+
+	if selection == '6':
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print('FTP client now exiting')
+		print(input('\nHit return to exit'))
+		break
+
+s.close()                  
