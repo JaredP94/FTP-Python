030e3d31011ec84fe059ade8f9050ced4314a315
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-03-08 18:10:22 +0200

    Interface

--- Code/interface.py
+++ Code/interface.py
@@ -38,24 +38,48 @@ class Example(QtGui.QMainWindow):
         ############# Create grid ####################
 
         backButtonServer = QtGui.QPushButton("Back")
-        forwardButtonServer = QtGui.QPushButton("Forward")
         download = QtGui.QPushButton("Download")
         upload = QtGui.QPushButton("Upload")
         backButtonClient = QtGui.QPushButton("Back")
-        forwardButtonClient = QtGui.QPushButton("Forward")
+        connectToServer = QtGui.QPushButton("Connect")
         grid = QtGui.QGridLayout()
-        grid = QtGui.QGridLayout()
-        grid.setSpacing(10) 
+        grid.setSpacing(12) 
 
         ################ Add widgets to grid ########################
+        
+        ip=QtGui.QLabel("IP Address")
+        ipAddress = QtGui.QLineEdit()
+        ipAddress.setFixedWidth(150)
 
+        userName=QtGui.QLabel("Username")
+        name = QtGui.QLineEdit()
+        name.setFixedWidth(150)
+	
+        password1=QtGui.QLabel("Password")
+        password = QtGui.QLineEdit()
+        password.setFixedWidth(150)
+        password.setEchoMode(QtGui.QLineEdit.Password)
+        grid.addWidget(ip, 0,0)
+        grid.addWidget(ipAddress, 0,1,1,2)
+        grid.addWidget(userName, 1,0,)
+        grid.addWidget(name, 1, 1,1,2)
+        grid.addWidget(password1, 2,0)
+        grid.addWidget(password, 2, 1,1,2)
+        grid.addWidget(connectToServer, 2,3)
+
+        # spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        # grid.addItem(spacerItem, 3, 0, 1, 8)
+
+        ######## Labels #########
+        server = QtGui.QLabel("Remote")
+        local= QtGui.QLabel("Local")
+        grid.addWidget(server, 4, 2,1,1)
+        grid.addWidget(local, 4, 8,1,1)
         ##### Buttons #####
-        grid.addWidget(backButtonServer, 1, 1, 1, 1)
-        grid.addWidget(forwardButtonServer, 1, 2)
-        grid.addWidget(download, 1, 3)
-        grid.addWidget(upload, 1, 7)
-        grid.addWidget(backButtonClient, 1, 8)
-        grid.addWidget(forwardButtonClient, 1, 9)
+        grid.addWidget(backButtonServer, 4, 0,1,1)
+        grid.addWidget(download, 4, 4,1,1)
+        grid.addWidget(upload, 4, 10,1,1)
+        grid.addWidget(backButtonClient,4, 6,1,1)
 
         ##### View Tree File System #####
         model = QtGui.QFileSystemModel()
@@ -70,7 +94,7 @@ class Example(QtGui.QMainWindow):
         # view.setRootIndex(model.index(QDir.currentPath()))
 
         ##### Add file system to grid #####
-        grid.addWidget(view, 2, 0, 5, 5)
+        grid.addWidget(view, 6, 0,5,5)
 
         ##### View Tree File System #####
         model2 = QtGui.QFileSystemModel()
@@ -78,14 +102,8 @@ class Example(QtGui.QMainWindow):
         view2 = QtGui.QTreeView()
         view2.setModel(model2)
 
-        # model = QtGui.QFileSystemModel()
-        # model.setRootPath(QDir.currentPath())
-        # view = QtGui.QTreeView()
-        # view.setModel(model)
-        # view.setRootIndex(model.index(QDir.currentPath()))
-
         ##### Add file system to grid #####
-        grid.addWidget(view2, 2, 6, 5, 5)
+        grid.addWidget(view2, 6, 6,5,5)
 
         ####### Create widgets in QMainWindow #######
         widget = QtGui.QWidget()
@@ -108,13 +126,31 @@ class Example(QtGui.QMainWindow):
             data = f.read()
             self.textEdit.setText(data) 
 
-    def openConnectWindow(self):      
+    def textchanged(self):
+        print "contents of text box: "+self.text
+
+    def openConnectWindow(self): 
         text, ok = QtGui.QInputDialog.getText(self, 'Connect to server', 
         'Enter the IP address to connect to:')
         
-        if ok:
-            self.le.setText(str(text))                           
+        if ok and text:
+            self.le.setText(str(text))
+            self.username   
+
+    def username(self):      
+        text2, ok = QtGui.QInputDialog.getText(self, 'Log in', 
+        'Enter your username:')
         
+        if ok:
+            self.le.setText(str(text2))                         
+
+    def password(self):      
+        text, ok = QtGui.QInputDialog.getText(self, 'Log in', 
+        'Enter your password:')
+        
+        if ok:
+            self.le.setText(str(text))  
+
 def main():
     
     app = QtGui.QApplication(sys.argv)
