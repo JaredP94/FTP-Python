9beac61fcfc37a446f0085f335612dfafbe751f9
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-03-09 15:50:53 +0200

    Connect to and disconnect from server in the interface

--- Code/interface.py
+++ Code/interface.py
@@ -4,36 +4,22 @@ from PyQt4.QtGui import (QApplication, QColumnView, QFileSystemModel,
                          QSplitter, QTreeView)
 from PyQt4.QtCore import QDir, Qt
 
-
+import socket
+import sys
+import os
+import time
+from time import sleep
+from os import walk
+# from client import recieve, action
+ 
 class Example(QtGui.QMainWindow):
     
     def __init__(self):
         super(Example, self).__init__()
-        
+
         self.initUI()
-        
+
     def initUI(self):      
-
-        ########## Create status bar ###########
-        self.statusBar()
-
-        openFile = QtGui.QAction('Open', self)
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open new File')
-        openFile.triggered.connect(self.showDialog)
-
-        connect = QtGui.QAction('Connect',self)
-        connect.setStatusTip("Connect to FTP server")
-        connect.triggered.connect(self.openConnectWindow)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
-        fileMenu = menubar.addMenu('&Connect')  
-        fileMenu.addAction(connect)     
-
-        ########## End create status bar ###########
-
 
         ############# Create grid ####################
 
@@ -42,39 +28,47 @@ class Example(QtGui.QMainWindow):
         upload = QtGui.QPushButton("Upload")
         backButtonClient = QtGui.QPushButton("Back")
         connectToServer = QtGui.QPushButton("Connect")
+        connectToServer.clicked.connect(self.connect_to_server)
+        disconnectFromServer = QtGui.QPushButton("Disconnect")
+        disconnectFromServer.clicked.connect(self.disconnect_from_server)
         grid = QtGui.QGridLayout()
         grid.setSpacing(12) 
 
         ################ Add widgets to grid ########################
         
         ip=QtGui.QLabel("IP Address")
-        ipAddress = QtGui.QLineEdit()
-        ipAddress.setFixedWidth(150)
+        self.ipAddress = QtGui.QLineEdit()
+        self.ipAddress.setFixedWidth(150)
+
+        portLabel=QtGui.QLabel("Port")
+        self.port = QtGui.QLineEdit()
+        self.port.setFixedWidth(150)
 
         userName=QtGui.QLabel("Username")
-        name = QtGui.QLineEdit()
-        name.setFixedWidth(150)
+        self.name = QtGui.QLineEdit()
+        self.name.setFixedWidth(150)
 	
         password1=QtGui.QLabel("Password")
-        password = QtGui.QLineEdit()
-        password.setFixedWidth(150)
-        password.setEchoMode(QtGui.QLineEdit.Password)
+        self.password = QtGui.QLineEdit()
+        self.password.setFixedWidth(150)
+        self.password.setEchoMode(QtGui.QLineEdit.Password)
         grid.addWidget(ip, 0,0)
-        grid.addWidget(ipAddress, 0,1,1,2)
+        grid.addWidget(self.ipAddress, 0,1,1,2)
+        grid.addWidget(portLabel, 0,3)
+        grid.addWidget(self.port,0,4,1,2)
         grid.addWidget(userName, 1,0,)
-        grid.addWidget(name, 1, 1,1,2)
+        grid.addWidget(self.name, 1, 1,1,2)
         grid.addWidget(password1, 2,0)
-        grid.addWidget(password, 2, 1,1,2)
+        grid.addWidget(self.password, 2, 1,1,2)
         grid.addWidget(connectToServer, 2,3)
-
-        # spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        # grid.addItem(spacerItem, 3, 0, 1, 8)
+        grid.addWidget(disconnectFromServer, 2,4)
 
         ######## Labels #########
         server = QtGui.QLabel("Remote")
         local= QtGui.QLabel("Local")
         grid.addWidget(server, 4, 2,1,1)
         grid.addWidget(local, 4, 8,1,1)
+
         ##### Buttons #####
         grid.addWidget(backButtonServer, 4, 0,1,1)
         grid.addWidget(download, 4, 4,1,1)
@@ -114,45 +108,39 @@ class Example(QtGui.QMainWindow):
         self.setGeometry(500, 500, 1000, 500)
         self.setWindowTitle('FTP Client')
         self.show()
-        
-    def showDialog(self):
 
-        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
-                '/home')
-        
-        f = open(fname, 'r')
-        
-        with f:        
-            data = f.read()
-            self.textEdit.setText(data) 
+    def connect_to_server(self):
+        self.s = socket.socket()
+        ip = self.ipAddress.text()
+        port=int(self.port.text())
+        username=self.name.text()
+        password=self.password.text()
 
-    def textchanged(self):
-        print "contents of text box: "+self.text
+        self.s.connect((ip, port))
+        self.s.recv(1024)
 
-    def openConnectWindow(self): 
-        text, ok = QtGui.QInputDialog.getText(self, 'Connect to server', 
-        'Enter the IP address to connect to:')
-        
-        if ok and text:
-            self.le.setText(str(text))
-            self.username   
+        self.action('USER '+'my_name_is_jeff')
+        self.action('PASS '+'strongpassword')
 
-    def username(self):      
-        text2, ok = QtGui.QInputDialog.getText(self, 'Log in', 
-        'Enter your username:')
-        
-        if ok:
-            self.le.setText(str(text2))                         
+    def disconnect_from_server(self):
+        self.ipAddress.clear()
+        self.port.clear()
+        self.name.clear()
+        self.password.clear()
+        self.s.close()
 
-    def password(self):      
-        text, ok = QtGui.QInputDialog.getText(self, 'Log in', 
-        'Enter your password:')
-        
-        if ok:
-            self.le.setText(str(text))  
+    def recieve(self):
+	    rec = self.s.recv(1024)
+	    return (rec)
+	    
+    def action(self,mes=''):
+	    self.send(mes)
+	    return self.recieve()
+
+    def send(self,mes=''):
+	    self.s.send(bytes(mes + ("\r\n"), "UTF-8"))       
 
 def main():
-    
     app = QtGui.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
