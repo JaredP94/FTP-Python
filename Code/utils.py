6436ebf33127d69be4308bfbedcde808b09cd3bf
Jared Ping <jaredping@yahoo.com>
2018-03-13 18:29:26 +0200

    Document utils.py

--- Code/utils.py
+++ Code/utils.py
@@ -5,40 +5,47 @@ import os
 import stat
 
 def fileProperty(filepath):
-    """
-    return information from given file, like this "-rw-r--r-- 1 User Group 312 Feb 27 2018 filename"
-    """
-    st = os.stat(filepath)
-    fileMessage = [ ]
-    def getFileMode( ):
+    # Returns information from given file, i.e: "-rw-r--r-- 1 User Group 312 Feb 27 2018 filename"
+    filepath_stat = os.stat(filepath)
+    file_info = []
+
+    def getFileMode():
         modes = [
             stat.S_IRUSR, stat.S_IWUSR, stat.S_IXUSR,
             stat.S_IRGRP, stat.S_IWGRP, stat.S_IXGRP,
             stat.S_IROTH, stat.S_IWOTH, stat.S_IXOTH,
         ]
-        mode     = st.st_mode
+        mode     = filepath_stat.st_mode
         fullmode = ''
         fullmode += os.path.isdir(filepath) and 'd' or '-'
 
-        for i in range(9):
-            fullmode += bool(mode & modes[i]) and 'rwxrwxrwx'[i] or '-'
+        for index in range(9):
+            fullmode += bool(mode & modes[index]) and 'rwxrwxrwx'[index] or '-'
+
         return fullmode
 
-    def getFilesNumber( ):
-        return str(st.st_nlink)
+    def getFilesNumber():
+        # Number of hard links
+        return str(filepath_stat.st_nlink)
 
-    def getUser( ):
-        return pwd.getpwuid(st.st_uid).pw_name
+    def getUser():
+        # File owner
+        return pwd.getpwuid(filepath_stat.st_uid).pw_name
 
-    def getGroup( ):
-        return grp.getgrgid(st.st_gid).gr_name
+    def getGroup():
+        # File group association
+        return grp.getgrgid(filepath_stat.st_gid).gr_name
 
-    def getSize( ):
-        return str(st.st_size)
+    def getSize():
+        # Size of file in bytes
+        return str(filepath_stat.st_size)
 
-    def getLastTime( ):
-        return time.strftime('%b %d %H:%M', time.gmtime(st.st_mtime))
-    for func in ('getFileMode()', 'getFilesNumber()', 'getUser()', 'getGroup()', 'getSize()', 'getLastTime()'):
-        fileMessage.append(eval(func))
-    fileMessage.append(os.path.basename(filepath))
-    return ' '.join(fileMessage)
+    def getLastTime():
+        # Time of most recent content modification
+        return time.strftime('%b %d %H:%M', time.gmtime(filepath_stat.st_mtime))
+
+    for function in ('getFileMode()', 'getFilesNumber()', 'getUser()', 'getGroup()', 'getSize()', 'getLastTime()'):
+        file_info.append(eval(function))
+
+    file_info.append(os.path.basename(filepath))
+    return ' '.join(file_info)
