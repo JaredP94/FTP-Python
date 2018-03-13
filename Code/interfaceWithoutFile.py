7987808dee2057659c64f219afcb7a9aa3039b6f
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-03-13 20:39:09 +0200

    Started commenting + added full list of ascii files

--- Code/interfaceWithoutFile.py
+++ Code/interfaceWithoutFile.py
@@ -35,10 +35,9 @@ class Example(QtGui.QMainWindow):
 
     def initUI(self):      
         style = QApplication.style()
-        # self.path = os.getcwd()
+
         self.path = str(Path.home())
         self.filePath = str(Path.home())
-        print(self.path)
         self.file_all = style.standardIcon(QtGui.QStyle.SP_FileIcon)
         self.dir_all = style.standardIcon(QtGui.QStyle.SP_DirIcon)
         self.setStyleSheet(styleSheet)
@@ -46,8 +45,11 @@ class Example(QtGui.QMainWindow):
         self.username=''
         self.newDir = ''
 
-        ############# Create grid ####################
+        # Create grid
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10) 
 
+        # Create buttons
         download = QtGui.QPushButton("Download")
         upload = QtGui.QPushButton("Upload")
         upload.clicked.connect(self.defineFileType)
@@ -58,11 +60,8 @@ class Example(QtGui.QMainWindow):
         disconnectFromServer.clicked.connect(self.disconnect_from_server)
         makeDirectory = QtGui.QPushButton("Create Directory")
         makeDirectory.clicked.connect(self.mkdir)
-        grid = QtGui.QGridLayout()
-        grid.setSpacing(10) 
 
-        ################ Add widgets to grid ########################
-        
+        # Create text boxes and labels
         ip=QtGui.QLabel("IP Address")
         self.ipAddress = QtGui.QLineEdit()
         self.ipAddress.setFixedWidth(150)
@@ -79,6 +78,8 @@ class Example(QtGui.QMainWindow):
         self.password = QtGui.QLineEdit()
         self.password.setFixedWidth(150)
         self.password.setEchoMode(QtGui.QLineEdit.Password)
+
+        # Add widgets to grid
         grid.addWidget(ip, 0,0,1,1)
         grid.addWidget(self.ipAddress, 0,1,1,2)
         grid.addWidget(portLabel, 0,3,1,1)
@@ -87,21 +88,21 @@ class Example(QtGui.QMainWindow):
         grid.addWidget(self.name, 1, 1,1,2)
         grid.addWidget(password1, 2,0,1,1)
         grid.addWidget(self.password, 2, 1,1,2)
-        grid.addWidget(connectToServer, 2,3,1,1)
-        grid.addWidget(disconnectFromServer, 2,4,1,1)
 
-        ######## Labels #########
+        # Labels
         server = QtGui.QLabel("Remote")
         local= QtGui.QLabel("Local")
         grid.addWidget(server, 3, 2,1,1)
         grid.addWidget(local, 3, 7,1,1)
 
-        ##### Buttons #####
+        # Buttons
         grid.addWidget(download, 3, 4,1,1)
         grid.addWidget(upload, 3, 9,1,1)
         grid.addWidget(makeDirectory, 3, 0,1,1)
+        grid.addWidget(connectToServer, 2,3,1,1)
+        grid.addWidget(disconnectFromServer, 2,4,1,1)
 
-        ####### Create manual file system for server ########
+        # Create file system for server
         self.view = QtGui.QTreeView()
         self.view.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
         self.view.clicked.connect(self.traverseTreeServer)
@@ -109,15 +110,15 @@ class Example(QtGui.QMainWindow):
         self.view.setModel(self.model)
         self.view.setUniformRowHeights(True)
 
-        ###### Stop the tree from being editable #######
+        # Stop the filesystem from being editable
         self.view.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.view.setHeaderHidden(True)
         self.view.setAlternatingRowColors(True)
 
-        ##### Add file system to grid #####
+        # Add file system to grid
         grid.addWidget(self.view, 4, 0,10,5)
 
-        ####### Create manual file system for local ########
+        # Create file system for local
         self.view2 = QtGui.QTreeView()
         self.view2.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
         self.view2.clicked.connect(self.traverseTreeClient)
@@ -125,31 +126,34 @@ class Example(QtGui.QMainWindow):
         self.view2.setModel(self.model2)
         self.view2.setUniformRowHeights(True)
 
-        ###### Stop the tree from being editable #######
+        # Stop the tree from being editable
         self.view2.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.view2.setHeaderHidden(True)
         self.view2.setAlternatingRowColors(True)
 
+        # Add file system to grid
         grid.addWidget(self.view2, 4, 5,10,5)
 
+        # Add textbox to log responses
         self.logOutput = QtGui.QTextEdit()
         self.logOutput.setReadOnly(True)
         self.logOutput.setLineWrapMode(QtGui.QTextEdit.NoWrap)
-        # grid.addWidget(self.logOutput, 15,0,1,10)
         grid.addWidget(self.logOutput, 0,7,3,3)
         response= QtGui.QLabel("Server Response")
         grid.addWidget(response, 0,6,1,1)
 
-        ####### Create widgets in QMainWindow #######
+        # Create widgets in QMainWindow
         widget = QtGui.QWidget()
         widget.setLayout(grid)
         self.setCentralWidget(widget)
 
-        ####### Window Properties #######
+        # Set Window Properties 
         self.setGeometry(500, 500, 1000, 500)
         self.setWindowTitle('FTP Client')
         self.show()
 
+    # Connects the client to the server and calls the functions
+    # to populate the filesystems
     def connect_to_server(self):
         self.s = socket.socket()
         ip = self.ipAddress.text()
@@ -162,14 +166,14 @@ class Example(QtGui.QMainWindow):
 
         self.action('USER '+ self.username)
         self.action('PASS '+ self.password)
-        # self.action('USER '+'my_name_is_jeff')
-        # self.action('PASS '+'strongpassword')
-        self.action('USER '+'group2')
-        self.action('PASS '+'ei9keNge')
+
+        # self.action('USER '+'group2')
+        # self.action('PASS '+'ei9keNge')
         self.pathServer = self.getPWDServer()
         self.updateTreeClient()
         self.updateTreeServer()
 
+    # Disconnects the client from the server
     def disconnect_from_server(self):
         self.ipAddress.clear()
         self.port.clear()
@@ -181,16 +185,17 @@ class Example(QtGui.QMainWindow):
         self.action('QUIT')
         self.s.close()
 
+    # Requests the server's current working directory and formats it
     def getPWDServer(self):
         mes = ('PWD')
         self.send(mes)
         directory = self.s.recv(1024)
         directory = directory.decode()
-        vali = directory.split('i')
-        vali = vali[0].split(' ')
-        vali = vali[0]
+        returnCode = directory.split('i')
+        returnCode = returnCode[0].split(' ')
+        returnCode = returnCode[0]
 
-        if vali == '257':
+        if returnCode == '257':
             directory = directory.split('"')
             directory = directory[1]
             print('"'+directory+'"')
@@ -199,90 +204,68 @@ class Example(QtGui.QMainWindow):
 
         return directory
 
-    def traverseTreeServer(self,index):
-        self.pathServer = self.getTreePathServer(index)
-        self.pathServer = "/" + self.pathServer
-        reply = self.action('CWD '+ self.pathServer)
-        time.sleep(.05)
-        while b'226' in reply:
-            reply=self.recieve()
-
-        print(reply)
-        if b'250' in reply:
-            self.updateTreeServer()
-            indexItem = self.model.index(index.row(), 0, index.parent())
-            self.fileNameServer = self.model.itemFromIndex(indexItem).text()
-            self.filePathServer = self.getTreePathServer(index)
-            self.model.removeRow(0)
-        elif b'550' in reply:
-            indexItem = self.model.index(index.row(), 0, index.parent())
-            self.fileNameServer = self.model.itemFromIndex(indexItem).text()
-            self.filePathServer = self.getTreePathServer(index)
-
-    def traverseTreeClient(self,index):
-        self.path = self.getTreePath(index)
-        self.path = "/" + self.path
-        if os.path.isdir(self.path):
-            self.updateTreeClient()
-            indexItem = self.model2.index(index.row(), 0, index.parent())
-            self.fileName = self.model2.itemFromIndex(indexItem).text()
-            self.filePath = self.getTreePath(index)
-            self.model2.removeRow(0)
-        else:
-            indexItem = self.model2.index(index.row(), 0, index.parent())
-            self.fileName = self.model2.itemFromIndex(indexItem).text()
-            self.filePath = self.getTreePath(index)
-
+    # Receives and decodes any required messages from th server
     def recieve(self):
         rec = self.s.recv(1024)
         message = rec.decode()
         self.printToLog(message)
         return (rec)
 
+    # Prints the reply messages from the server onto the GUI
     def printToLog(self,message):
         self.logOutput.moveCursor(QtGui.QTextCursor.End)
         self.logOutput.insertPlainText(message)
         self.sb = self.logOutput.verticalScrollBar()
         self.sb.setValue(self.sb.maximum()) 
-        # if '530' or '421' in message:
-        #     self.action('USER '+ self.username)
-        #     self.action('PASS '+ self.password)  
 
+    # Sends a message using the send() function and calls the receive() 
+    # function to receive the reply
     def action(self,mes=''):
 	    self.send(mes)
 	    return self.recieve()
 
+    # Send a message to the server
     def send(self,mes=''):
 	    self.s.send(bytes(mes + ("\r\n"), "UTF-8"))  
 
+    # Informs the server that passive mode should be used
+    # Extracts the IP address and port from the server reply 
     def pasv(self):
-	    while True:
-		    vali = ''
-		    mes = ('PASV')
-		    self.send(mes)
-		    mes = self.recieve()
-		    mes = mes.decode()
-		    nmsg = mes.split('(')
-		    nmsg = nmsg[-1].split(')')
-		    p = nmsg[0].split(',')
-		    newip = '.'.join(p[:4])
-		    newport = int(p[4])*256 + int(p[5])
-		    return (newip,newport)
-		    break
+        while True:
+            vali = ''
+            mes = ('PASV')
+            mes = self.action(mes)
+            mes = mes.decode()
+            nmsg = mes.split('(')
+            nmsg = nmsg[-1].split(')')
+            p = nmsg[0].split(',')
+            newip = '.'.join(p[:4])
+            newport = int(p[4])*256 + int(p[5])
+            return (newip,newport)
+            break
 
+    # Function to return the current path of the local machine
     def local_dir(self,path=''):
         for (dirpath, dirnames, filenames) in walk(path):
             path = dirpath
             break   
         return path
 
+    # Function to return the directories and files contained
+    # within the current path
     def browse_local(self,path=''):
         for (dirpath, dirnames, filenames) in walk(path):
             break
         return (dirnames, filenames)
 
     def findExtension(self, filename):
-        listOfAscii = ['html', 'php', 'cgi', 'js','txt','css']
+        listOfAscii = ['as','asm','asp','atom','atomcat','c','cc',
+        'cfm','cgi','conf','cpp','css','csv','def','dhtml','dic','dtd','ecma',
+        'es6','es7','f','f77','f90','for','h','hh','hpp','htm','html','java',
+        'jhtml','js','js','json','list','log','m','md','mm','opml','p','pas',
+        'php','phtml','pl','rb','rss','rt','rtf','rtx','ruby','s','sgm','sgml',
+        'sh','shtml','sql','swift','text','tsv','txt','vbs','vcf','vcs','webapp',
+        'wri','xht','xhtml','xml','xsl','xsl','xslt','xspf']
         filetype, encoding = mimetypes.guess_type(filename)
         for i in listOfAscii:
             if i in filetype:
@@ -290,7 +273,8 @@ class Example(QtGui.QMainWindow):
         return 'TYPE I'
 
     def defineDownloadType(self):
-        self.filePathServer="/" + self.filePathServer
+        print ("Filepathserver: ")
+        print(self.filePathServer)
         mes= self.findExtension(self.fileNameServer)
         reply= self.send(mes)
         while True:
@@ -449,6 +433,50 @@ class Example(QtGui.QMainWindow):
         p.close()
         return (folders,files)
 
+# Open modal for the user to insert file name of file to be created
+    def mkdir(self):      
+        text, ok = QtGui.QInputDialog.getText(self, 'Create Directory', 
+        'Enter the directory name:')
+        
+        if ok:
+            self.newDir=(str(text))
+            self.makeNewDirectory()  
+
+# Create a new directory on the server
+    def makeNewDirectory(self):
+        filePath = self.pathServer
+        print("path in makeDirectory: ")
+        print (filePath)
+        if not os.path.isdir(filePath):
+            index=filePath.rfind('/')
+            filePath=filePath[:index]
+
+        filePath = filePath + '/' + self.newDir
+        self.action('MKD '+filePath)
+
+    def getTreePath(self, index):
+        path = []
+        while index.isValid():
+            indexItem = self.model2.index(index.row(), 0, index.parent())
+            name = self.model2.itemFromIndex(indexItem).text()
+            path.append(name)
+            index = index.parent()
+        return '/'.join(reversed(path))
+
+    def traverseTreeClient(self,index):
+        self.path = self.getTreePath(index)
+        self.path = "/" + self.path
+        if os.path.isdir(self.path):
+            self.updateTreeClient()
+            indexItem = self.model2.index(index.row(), 0, index.parent())
+            self.fileName = self.model2.itemFromIndex(indexItem).text()
+            self.filePath = self.getTreePath(index)
+            self.model2.removeRow(0)
+        else:
+            indexItem = self.model2.index(index.row(), 0, index.parent())
+            self.fileName = self.model2.itemFromIndex(indexItem).text()
+            self.filePath = self.getTreePath(index)
+
     def updateTreeClient(self):
         ####### Get directory structure #######
         pathText = self.local_dir(self.path)
@@ -489,15 +517,6 @@ class Example(QtGui.QMainWindow):
         self.model2.appendRow(child[0])
         self.view2.expandAll()
 
-    def getTreePath(self, index):
-        path = []
-        while index.isValid():
-            indexItem = self.model2.index(index.row(), 0, index.parent())
-            name = self.model2.itemFromIndex(indexItem).text()
-            path.append(name)
-            index = index.parent()
-        return '/'.join(reversed(path))
-
     def getTreePathServer(self, index):
         path = []
         while index.isValid():
@@ -507,8 +526,31 @@ class Example(QtGui.QMainWindow):
             index = index.parent()
         return '/'.join(reversed(path))
 
+    def traverseTreeServer(self,index):
+        self.pathServer = self.getTreePathServer(index)
+        print("Path in traverse server ")
+        print(self.pathServer)
+        if self.pathServer[0]=='/' and self.pathServer[1]=='/':
+            self.pathServer=self.pathServer[1:]
+
+        reply = self.action('CWD '+ self.pathServer)
+        time.sleep(.05)
+        while b'226' in reply:
+            reply=self.recieve()
+
+        if b'250' in reply:
+            self.updateTreeServer()
+            indexItem = self.model.index(index.row(), 0, index.parent())
+            self.fileNameServer = self.model.itemFromIndex(indexItem).text()
+            self.filePathServer = self.getTreePathServer(index)
+            self.model.removeRow(0)
+        elif b'550' in reply:
+            indexItem = self.model.index(index.row(), 0, index.parent())
+            self.fileNameServer = self.model.itemFromIndex(indexItem).text()
+            self.filePathServer = self.getTreePathServer(index)
+
     def updateTreeServer(self):
-        ####### Get directory structure #######
+        # Get current path of server
         pathText = self.getPWDServer()
 
         print("Path in update " + pathText)
@@ -517,14 +559,13 @@ class Example(QtGui.QMainWindow):
             newPath = pathText
 
         elif pathText[0]=="/":
-            pathText = pathText[1:]
             newPath = pathText.split('/')
             print(newPath)
             newPath= [x.strip() for x in newPath if x.strip()]
+            newPath.insert(0, '/')
 
         counter = 0
         child=[]
-
         for i in newPath:
             child.append(QtGui.QStandardItem(i)) # item 0 is the parent
             child[counter].setIcon(self.dir_all)
@@ -552,26 +593,6 @@ class Example(QtGui.QMainWindow):
         ##### Last step: Add the tree to the model ######
         self.model.appendRow(child[0])
         self.view.expandAll()
-
-    def mkdir(self):      
-        text, ok = QtGui.QInputDialog.getText(self, 'Create Directory', 
-        'Enter the directory name:')
-        
-        if ok:
-            self.newDir=(str(text))
-            self.makeNewDirectory()  
-
-    def makeNewDirectory(self):
-        filePath = self.pathServer
-        print("path in makeDirectory: ")
-        print (filePath)
-        if not os.path.isdir(filePath):
-            index=filePath.rfind('/')
-            filePath=filePath[:index]
-
-        filePath = filePath + '/' + self.newDir
-        self.action('MKD '+filePath)
-
             
 def main():
     app = QtGui.QApplication(sys.argv)
